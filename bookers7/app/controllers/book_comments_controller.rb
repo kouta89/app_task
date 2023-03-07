d0dff910ac79586669ecbcadb75d0226ed2693f9
geromon <123849679+geromon89@users.noreply.github.com>
2023-03-07 13:35:58 +0000

    app_task-7

---
+++ bookers7/app/controllers/book_comments_controller.rb
@@ -0,0 +1,28 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      flash.now[:notice] = 'コメントを投稿しました'
+      render :book_comments
+    else
+      render 'books/show'
+    end
+
+  end
+
+  def destroy
+    BookComment.find_by(id: params[:id],book_id: params[:book_id]).destroy
+    flash.now[:alert] = '投稿を削除しました'
+    @book = Book.find(params[:book_id])
+    render :book_comments
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
