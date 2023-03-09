34e70bfd1a3251d006ca4cf1cbcbeeb35294e86d
geromon <123849679+geromon89@users.noreply.github.com>
2023-03-09 09:22:40 +0000

    showの表示がおかしい

--- bookers789a/app/controllers/users_controller.rb
+++ bookers789a/app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update,:edit]
+  before_action :ensure_correct_user, only: [:show,:update,:edit]
 
   def show
     @user = User.find(params[:id])
+    @current_entry = Entry.where(user_id: current_user.id)
+    @another_entry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @current_entry.each do |cu|
+        @another_entry.each do |an|
+          if cu.room_id == an.room_id
+            @is_room = true
+            @room_id = cu.room_id
+          end
+        end
+      end
+      unless @is_room
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
     @books = @user.books
     @book = Book.new
   end
--- bookers789a/app/models/user.rb
+++ bookers789a/app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :entries, dependent: :destroy
   has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
 
   # フォローをした、されたの関係
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
